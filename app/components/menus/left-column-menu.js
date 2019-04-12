8ff2b7ff4d78b95497fac9934625684050e9a4ae
trideep.k <trideep.k@talview.com>
2019-04-12 22:26:56 +0530

    active class for scroll-to helper added

--- app/components/menus/left-column-menu.js
+++ app/components/menus/left-column-menu.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didInsertElement() {
+    this.send('setActive', this.get('menuItems.firstObject'))
+  },
+
+  actions: {
+    setActive(item) {
+      this.set('selectedContent', item.route)
+    }
+  }
 });
