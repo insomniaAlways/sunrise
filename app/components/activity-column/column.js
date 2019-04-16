d931fbc37774371ebbbfb3d5374ee3cf81532981
trideep.k <trideep.k@talview.com>
2019-04-16 21:55:26 +0530

    event column added

--- app/components/activity-column/column.js
+++ app/components/activity-column/column.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
+  store: Ember.inject.service(),
+
+  events: Ember.computed(function() {
+    return this.get('store').findAll('event')
+  })
 });
