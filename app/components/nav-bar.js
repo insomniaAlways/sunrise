fb761ba1fd38b1a8fdb8de14d25bca3afbd8a5ba
trideep.k <trideep.k@talview.com>
2019-05-30 09:31:05 +0530

    route to home added

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -11,7 +11,7 @@ export default Component.extend({
   }),
   actions: {
     invalidateSession() {
-      this.get('session').invalidate().then(() => window.location.replace('localhost:4200/home'));
+      this.get('session').invalidate().then(() => this.get('router').transitionTo('home'));
     },
     toggleMenu() {
       Ember.$('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
